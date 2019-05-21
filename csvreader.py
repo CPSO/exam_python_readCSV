8d9bd2ee74f023cc0d2e90fa242f548c03a47eab
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2019-05-21 11:00:49 +0200

    basic search function added

--- csvreader.py
+++ csvreader.py
@@ -15,8 +15,12 @@ def main():
 def setupMenu():
     menu = ConsoleMenu("K.E.A - Krime Enforcment Archive")
     function_item = FunctionItem("Show the Data", showDB)
+    function_item2 = FunctionItem("Search the Data", searchCrime)
     menu.append_item(function_item)
+    menu.append_item(function_item2)
     menu.show()
+    
+    
        
 # Scripts
 def showDB():
@@ -31,6 +35,38 @@ def showDB():
             line_count += 1
         print(f'Processed {line_count} lines.')
     Screen().input('Press [Enter] to continue')
+    
+
+def searchCrime():
+    crimeCode = input('what code?: ')
+    with open('db/crimedb.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                 print(f'Column names are {", ".join(row)}')
+                 line_count += 1
+            if row['district'] == crimeCode:
+                print(f'\t{row["cdatetime"]} works in the {row["address"]} department, and was born in {row["district"]}.')
+                line_count += 1
+        print(f'Processed {line_count} lines.')
+        Screen().input('Press [Enter] to continue')
+       
+
+    """
+    ucr_ncic_codee = 2142
+    with open('db/crimedb.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            if row["ucr_ncic_codee"] == ucr_ncic_codee:
+                if line_count == 0:
+                    print(f'Column names are {", ".join(row)}')
+                    line_count += 1
+            print(f'\t {row}')
+            line_count += 1
+        print(f'Processed {line_count} lines.')
+        """
     
 
 main()
